b3797fadb04f4c9ed622be939ccb1220b1f92fe9
Benjamin Roger <llyrian@gmail.com>
2020-11-07 14:19:50 +0100

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,46 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default function IndexPage() {
+import _ from '../lib/i18n'
+
+export default function IndexPage({locale, locales, defaultLocale }) {
+
+
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
+      <h1>Index page</h1>
+
+      <p>i18n function tr: {_("testString")}</p>
+      <p>i18n function no tr: {_("errorString")}</p>
+      <p>i18n function missing: {_("errorStrin")}</p>
+
+      <p>Current locale: {locale}</p>
+      <p>Default locale: {defaultLocale}</p>
+      <p>Configured locales: {JSON.stringify(locales)}</p>
+
+      <Link href="/gsp">
+        <a>To getStaticProps page</a>
       </Link>
+      <br />
+
+      <Link href="/gsp/first">
+        <a>To dynamic getStaticProps page</a>
+      </Link>
+      <br />
+
+      <Link href="/gssp">
+        <a>To getServerSideProps page</a>
+      </Link>
+      <br />
     </div>
   )
+}
+
+export const getStaticProps = ({ locale, locales, defaultLocale }) => {
+  return {
+    props: {
+      locale,
+      locales,
+    },
+  }
 }
