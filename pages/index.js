4b485f9a4842021e6c0247d3eec11f5d506a89b6
Benjamin Roger <llyrian@gmail.com>
2020-11-08 17:46:22 +0100

    Included Github API fail case

--- pages/index.js
+++ pages/index.js
@@ -59,6 +59,15 @@ export default function IndexPage({ dataGitHub }) {
   </ul>);
 
 
+  if (dataGitHub) {
+    dataGitHub.sort(function (a, b) {
+      return new Date(b.updated_at) - new Date(a.updated_at);
+    });
+  } else {
+    dataGitHub = 0
+  };
+
+
   return (
     <>
       <Head>
@@ -106,14 +115,6 @@ export async function getServerSideProps(context) {
   let resGitHub = await fetch(config.githubAPI);
 
   var dataGitHub = await resGitHub.json();
-
-  if (dataGitHub) {
-    dataGitHub.sort(function (a, b) {
-      return new Date(b.updated_at) - new Date(a.updated_at);
-    });
-  } else {
-    dataGitHub = []
-  };
 
   return {
     props: {
