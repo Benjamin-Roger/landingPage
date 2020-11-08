0d8145cbc8034524da3a475a8c6a517958a6ebc0
Benjamin Roger <llyrian@gmail.com>
2020-11-08 12:24:13 +0100

    Added key to GithubContainer

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const LAST_NAME = 'Roger';
 const GITHUB_USERNAME = 'Benjamin-Roger';
 const WHATSAPP_HANDLE = '33658351332';
 const LINKEDIN_USERNAME = 'b-roger';
+const MALT_URL =  `https://www.malt.fr/profile/benjaminroger1`;
 const OFFICIAL_WEBSITE = 'https://www.sapiowork.com';
 const RESUME = 'https://resume.benjaminroger.com'
 const EMAIL = 'benjamin.roger@sapiowork.com';
@@ -15,38 +16,50 @@ module.exports = {
     lastName: LAST_NAME,
     fullName: `${FIRST_NAME} ${LAST_NAME}`,
     githubAPI: `https://api.github.com/users/Benjamin-Roger/repos`,
-    maltUrl: `https://www.malt.fr/profile/benjaminroger1`,
     socialLinks: [
         {
             source: 'fab',
             icon: 'github',
             name: 'Github',
             url: `https://github.com/${GITHUB_USERNAME}`,
+            color: `#000`
         },
         {
             source: 'fab',
             icon: 'linkedin',
             name: 'Linkedin',
             url: `https://linkedin.com/in/${LINKEDIN_USERNAME}`,
+            color: `#0e76a8`
         },
         {
             source: 'fab',
             icon: 'whatsapp',
             name: 'Whatsapp',
             url: `https://wa.me/${WHATSAPP_HANDLE}`,
-        },],
-    socialLinksTranslate: [
+            color: `#075E54`
+        },
+        {
+            source: 'img',
+            img: '/images/malt.png',
+            name: 'Malt',
+            url: `${MALT_URL}`,
+            color:`#FF5C57`
+        },
         {
             source: 'fas',
             icon: 'globe-europe',
-            name: 'onlineSite',
+            translatableString: 'onlineSite',
             url: `${OFFICIAL_WEBSITE}`,
+            color:`linear-gradient(90deg,#037657,#0fd19e 75%)`
         },
         {
-            name: 'onlineResume',
+            source: 'fas',
+            icon: 'file',
+            translatableString: 'onlineResume',
             url: `${RESUME}`,
+            color: `#28407F`
         },
-    ],
+    ]
 
 
 }
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Head from 'next/head'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
+import { faGlobeEurope, faFile } from '@fortawesome/free-solid-svg-icons';
 
-library.add({ faGithub, faLinkedin, faWhatsapp, faGlobeEurope });
+library.add({ faGithub, faLinkedin, faWhatsapp, faGlobeEurope, faFile });
 
 
 import _ from '@/libs/i18n'
@@ -15,22 +15,43 @@ import config from '../config.js';
 import { GithubItem } from '@/components/GithubItem.js';
 
 
-const GithubContainer = ({ dataGitHub, key }) => {
+const GithubContainer = ({ dataGitHub }) => {
 
   return <ul className="md:overflow-y-scroll mb-5">
-    {dataGitHub.map(repo => <GithubItem key={`git${key}`} {...repo} />)}
+    {dataGitHub.map((repo, key) => <GithubItem key={`git${key}`} {...repo} />)}
   </ul>;
 
 }
 
 export default function IndexPage({ dataGitHub }) {
 
-  const maltContainer = (<>
-    <p className="mb-2">{_("maltIntro")}</p>
-    <a href={config.maltUrl} title="Malt" alt="Malt" rel="nofollow" className="inline-block duration-150 ease-in-out hover:animate-pulse">
-      <img style={{ backgroundColor: '#FF5C57', maxHeight: "80px" }} className="rounded p-5" alt="Malt" title="Malt" src="/images/malt.png" />
-    </a>
-  </>);
+  const iconContainer = (<ul className="flex justify-around flex-wrap mt-3">
+    {config.socialLinks.map((link,key) =>
+      <li
+        style={{
+          height: "inherit",
+          width: "150px",
+          background: link.color
+        }}
+        key={`link-${key}`}
+        className="block rounded mx-3 my-2 hover:animate-pulse flex-grow">
+
+        <a href={link.url} title={link.name} alt={link.name} target="_blank" className="block w-full h-full p-5 text-center ">
+          {link.icon ?
+            <FontAwesomeIcon className="text-5xl text-white" icon={[link.source, link.icon]} />
+            : ''}
+          {link.source === 'img' ?
+            <img style={{ maxWidth: "100px" }} className="mt-4 mx-auto" alt="Malt" title="Malt" src={link.img} />
+            : ''}
+          <p className="text-white mt-2 mb-1">
+            {link.name || _(link.translatableString)}
+          </p>
+        </a>
+
+      </li>
+    )}
+
+  </ul>);
 
 
   return (
@@ -50,31 +71,7 @@ export default function IndexPage({ dataGitHub }) {
 
             <p>{_("intro")}</p>
 
-            <ul>
-              {config.socialLinks.map((link, key) =>
-                <li className="ml-3 hover:ml-5 transition-all ease duration-150 my-1" key={key}>
-                  <a href={link.url} title={link.name}>
-                    {link.icon ?
-                      <FontAwesomeIcon className="text-xl mr-2 text-blue-900" icon={[link.source, link.icon]} />
-                      : ''}
-                    {link.name}
-                  </a>
-                </li>)}
-              {config.socialLinksTranslate.map((link, key) =>
-                <li className="ml-3 hover:ml-5 transition-all ease duration-150 my-1" key={key}>
-                  <a href={link.url} title={link.name}>
-                    {link.icon ?
-                      <FontAwesomeIcon className="text-xl mr-2 text-blue-900" icon={[link.source, link.icon]} />
-                      : ''}
-                    {_(link.name)}
-                  </a>
-                </li>)}
-            </ul>
-
-            <hr className="border-teal-700 my-3" />
-
-            {maltContainer}
-
+            {iconContainer}
 
 
           </div>
@@ -99,7 +96,7 @@ export async function getServerSideProps(context) {
   let resGitHub = await fetch(config.githubAPI);
 
   var dataGitHub = await resGitHub
-  .json();
+    .json();
 
   await dataGitHub.sort(function (a, b) {
     return new Date(b.updated_at) - new Date(a.updated_at);
