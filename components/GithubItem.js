7f1f7df736a6aeec972d896b3b7d3fb4fc052d10
Benjamin Roger <llyrian@gmail.com>
2020-11-08 19:29:45 +0100

    Add loading animation

--- components/GithubItem.js
+++ components/GithubItem.js
@@ -5,11 +5,18 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 import _ from '@/libs/i18n';
 
+const placeholder = (width) => (
+    <span className={`h-6 w-${width || 32} block bg-teal-700 animate-pulse rounded my-2`} ></span>
+);
 
-export const GithubItem = ({ name, html_url, description, homepage }) => (
+export const GithubItem = ({ name, html_url, description, homepage, loading }) => (
+
     <li className="github__item block shadow my-2 p-4 rounded hover:ml-3 transition-all ease duration-150 hover:bg-teal-100">
-        <h3 className="font-extrabold text-teal-700"><a href={html_url} title={name} rel="nofollow">{name}</a></h3>
-        <p>{description}</p>
+        {!loading ?
+            <h3 className="font-extrabold text-teal-700"><a href={html_url} title={name} rel="nofollow">{name}</a></h3>
+            : placeholder()
+        }
+        <p>{!loading ? description : <>{placeholder(72)}</>}</p>
         {homepage ? <p className="text-right mt-3">
             <a
                 className="github__homepage font-semibold text-teal-700 "
--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,22 @@ const GithubContainer = () => {
     </>
   );
 
+  // if (!data) return (
+  //   <p className="mt-3 flex">
+  //     <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+  //       <circle class="opacity-25" cx="12" cy="12" r="10" stroke="teal" fill="none" stroke-width="4"></circle>
+  //       <path class="opacity-75" fill="teal" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  //     </svg>
+  //     Loading...
+  //   </p>
+  // );
+
   if (!data) return (
-    <p className="mt-3 flex">
-      <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="teal" fill="none" stroke-width="4"></circle>
-        <path class="opacity-75" fill="teal" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
-      Loading...
-    </p>
+    <ul className="mb-5 mr-4">
+     <GithubItem loading={true} />
+     <GithubItem loading={true} />
+     <GithubItem loading={true} />
+    </ul>
   );
 
   if (data) {
